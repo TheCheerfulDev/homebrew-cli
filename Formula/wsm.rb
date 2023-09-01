be261b99217d2d46f567b4a8d46c9f01c615d8a7
thecheerfuldev <mark@markhendriks.nl>
2023-09-01 20:33:22 +0200

    Updated wsm to 1.0.2

--- Formula/wsm.rb
+++ Formula/wsm.rb
@@ -1,8 +1,8 @@
 class Wsm < Formula
   desc "Manage your terminal workspaces"
   homepage "https://github.com/thecheerfuldev/workspace-manager"
-  url "https://github.com/thecheerfuldev/workspace-manager/archive/1.0.1.tar.gz"
-  sha256 "aa3505b41a883becde59a8822d3382a73098070b2d31d6ea4dd93666b7487b0e"
+  url "https://github.com/thecheerfuldev/workspace-manager/archive/1.0.2.tar.gz"
+  sha256 "3eaf2bb71faa27b3255ddfb1b0dfc81dd0b98e3262df268be2c3616b26e4bdd6"
   license "Apache-2.0"
 
   head "https://github.com/thecheerfuldev/workspace-manager.git"
@@ -17,8 +17,8 @@ class Wsm < Formula
         To activate workspace-manager, add the following lines to your `.bashrc` or `.zshrc` file:
 
         export WSM_WORKSPACES_DIRECTORY="</path/to/wsm_workspaces_directory>"
-        source <(workspace-manager sourcer)
-        source <(workspace-manager completion)
+        source <(wsm sourcer)
+        source <(wsm completion)
 
         The `wsm_workspaces_directory` is the directory where you will store your workspaces.
         Use the following structure:
