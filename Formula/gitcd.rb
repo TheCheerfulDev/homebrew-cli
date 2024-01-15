2c8d39dbcb79f8e003bf7a2951d217d2cea7ef6b
thecheerfuldev <mark@markhendriks.nl>
2024-01-15 19:57:03 +0100

    Updated gitcd to 0.1.0

--- Formula/gitcd.rb
+++ Formula/gitcd.rb
@@ -2,29 +2,29 @@ class Gitcd < Formula
   desc "Index and navigate to your git projects"
   homepage "https://github.com/thecheerfuldev/gitcd-go"
   license "Apache-2.0"
-  version "0.0.6"
+  version "0.1.0"
 
   head "https://github.com/thecheerfuldev/workspace-manager.git"
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.0.6/gitcd-go-0.0.6-darwin-arm64.tar.gz"
-      sha256 "f2f07dff543959d6690063d9db7b395239aefd0a6f6964cb6052e24b23b3ee70"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.1.0/gitcd-go-0.1.0-darwin-arm64.tar.gz"
+      sha256 "e884b0f384cf77ea27ac3d2259dcd2c0c38df3da3298b97ec2f877fa401733b1"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.0.5/gitcd-go-0.0.5-darwin-amd64.tar.gz"
-      sha256 "d9969013e84e549bae888b2025b16cef240840b3ea2a61676ce8a22411e17751"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.1.0/gitcd-go-0.1.0-darwin-amd64.tar.gz"
+      sha256 "acbc8aff46729d43314f7dd8bf43ddba23f1788278eec8cab695e03167a50683"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.0.5/gitcd-go-0.0.5-linux-arm64.tar.gz"
-      sha256 "eca7fafceca46d9a81e7f8f20d52c34a4558c2c8a8ecf4dafb93354d427cf0ed"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.1.0/gitcd-go-0.1.0-linux-arm64.tar.gz"
+      sha256 "708d7372c9971d87e93cc359b84c919e47e10cfa9f53a5e3cfa2677906195114"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.0.5/gitcd-go-0.0.5-linux-amd64.tar.gz"
-      sha256 "d9969013e84e549bae888b2025b16cef240840b3ea2a61676ce8a22411e17751"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.1.0/gitcd-go-0.1.0-linux-amd64.tar.gz"
+      sha256 "e6b212ca569d7b00805c90e94d37ff846531bfce7cb4695c506cb470b85f7f19"
     end
   end
 
