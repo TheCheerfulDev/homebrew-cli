dff93237dae20b29f9623d416f186663f3447ebb
thecheerfuldev <mark@markhendriks.nl>
2024-01-16 12:07:06 +0100

    Updated gitcd to 0.1.0

--- Formula/gitcd.rb
+++ Formula/gitcd.rb
@@ -2,29 +2,29 @@ class Gitcd < Formula
   desc "Index and navigate to your git projects"
   homepage "https://github.com/thecheerfuldev/gitcd-go"
   license "Apache-2.0"
-  version "0.1.0"
+  version "0.2.0"
 
   head "https://github.com/thecheerfuldev/workspace-manager.git"
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.1.0/gitcd-go-0.1.0-darwin-arm64.tar.gz"
-      sha256 "e884b0f384cf77ea27ac3d2259dcd2c0c38df3da3298b97ec2f877fa401733b1"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.2.0/gitcd-go-0.2.0-darwin-arm64.tar.gz"
+      sha256 "83f0209b5fdedafddba2ffa93488d6de2062971465413ce41edf8ac60e30ed0a"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.1.0/gitcd-go-0.1.0-darwin-amd64.tar.gz"
-      sha256 "acbc8aff46729d43314f7dd8bf43ddba23f1788278eec8cab695e03167a50683"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.2.0/gitcd-go-0.2.0-darwin-amd64.tar.gz"
+      sha256 "ad668b6192bc7d78156a1c9bce01057836c3da96ad7c1524adff50bbd800ea89"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.1.0/gitcd-go-0.1.0-linux-arm64.tar.gz"
-      sha256 "708d7372c9971d87e93cc359b84c919e47e10cfa9f53a5e3cfa2677906195114"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.2.0/gitcd-go-0.2.0-linux-arm64.tar.gz"
+      sha256 "2d3201c952da07df0bcaf08018883430ba520169b1da32b53fb9ba850aa7dd4d"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.1.0/gitcd-go-0.1.0-linux-amd64.tar.gz"
-      sha256 "e6b212ca569d7b00805c90e94d37ff846531bfce7cb4695c506cb470b85f7f19"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/0.2.0/gitcd-go-0.2.0-linux-amd64.tar.gz"
+      sha256 "23055961e69fe0981967dd860897c56690850b030e5062e1470c2d1dcc79834a"
     end
   end
 
@@ -38,14 +38,12 @@ class Gitcd < Formula
       To activate gitcd you need to add the following to your profile:
       
       export GITCD_PROJECT_HOME=<your root projects folder here>
-      alias gitcd="source $(which gitcd-go-runner.sh)"
+      alias gcd="source gitcd-go-runner.sh"
 
       OR pick any alias that you prefer :)
 
+      NOTE: If GITCD_PROJECT_HOME is NOT set, your home directory will be used instead.
+
     EOS
   end
-
-  #   test do
-  #     shell_output("eval \"$(#{bin}/jenv init -)\" && jenv versions")
-  #   end
 end
