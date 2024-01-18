71106bf2ec97f2890c7b69483d7ac3b80b9908cd
thecheerfuldev <mark@markhendriks.nl>
2024-01-18 19:05:03 +0100

    Updated Menv 0.3.0

--- Formula/menv.rb
+++ Formula/menv.rb
@@ -1,6 +1,6 @@
 class Menv < Formula
   desc "Manage your mvn configurations"
-  homepage "https://github.com/thecheerfuldev/menv-go"
+  homepage "https://github.com/thecheerfuldev/menv"
   license "Apache-2.0"
   version "0.3.0"
 
@@ -8,22 +8,22 @@ class Menv < Formula
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/menv-go/releases/download/0.3.0/menv-go-0.3.0-darwin-arm64.tar.gz"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.3.0/menv-0.3.0-darwin-arm64.tar.gz"
       sha256 "292ea466ec9c662d4aad1f1b94d37f9d479a3e419671d981d4805dfd7989bc04"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/menv-go/releases/download/0.3.0/menv-go-0.3.0-darwin-amd64.tar.gz"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.3.0/menv-0.3.0-darwin-amd64.tar.gz"
       sha256 "a54af4b05a541a049fb9e77e8da11dd4d844590c5eed80ccbba20d33d35e6090"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/menv-go/releases/download/0.3.0/menv-go-0.3.0-linux-arm64.tar.gz"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.3.0/menv-0.3.0-linux-arm64.tar.gz"
       sha256 "b5fe47e4a0419bfe880de0f6f08c9e94fdbc1cd993fca75c4037f47211bd7103"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/menv-go/releases/download/0.3.0/menv-go-0.3.0-linux-amd64.tar.gz"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.3.0/menv-0.3.0-linux-amd64.tar.gz"
       sha256 "6c353e812acfe123e4e3b5f5383fd3b0fbfb6aea0e786d3aa7017e37a1952b02"
     end
   end
