0262e9cdce6aa380812b1345013b015fcec6912d
thecheerfuldev <mark@markhendriks.nl>
2024-01-19 10:41:34 +0100

    Updated Menv 0.6.0

--- Formula/menv.rb
+++ Formula/menv.rb
@@ -2,29 +2,29 @@ class Menv < Formula
   desc "Manage your mvn configurations"
   homepage "https://github.com/thecheerfuldev/menv"
   license "Apache-2.0"
-  version "0.5.0"
+  version "0.6.0"
 
   head "https://github.com/thecheerfuldev/workspace-manager.git"
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/menv/releases/download/0.5.0/menv-0.5.0-darwin-arm64.tar.gz"
-      sha256 "e45de7fb80d3914b7d934efc38046a18535e62e2a80916550f333e04f616f1a0"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.6.0/menv-0.6.0-darwin-arm64.tar.gz"
+      sha256 "2fd6b402a145f4b5c7c28dad201c65f6153f713fbd284ad1ced762d5ad7e64bf"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/menv/releases/download/0.5.0/menv-0.5.0-darwin-amd64.tar.gz"
-      sha256 "d6d31c6b96b406544d5c7de8bccfc1cc68b29cbb807e70cee644830ed3d4e4ff"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.6.0/menv-0.6.0-darwin-amd64.tar.gz"
+      sha256 "84d6c9ee43774d228a9e7f3e0a4129ace1a6ad0985d8220944ee1525abe1fedf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/menv/releases/download/0.5.0/menv-0.5.0-linux-arm64.tar.gz"
-      sha256 "c7e36365b4c95f6d5ae87cb24c875858b52552b6e753229ac6979ad8e22c90e6"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.6.0/menv-0.6.0-linux-arm64.tar.gz"
+      sha256 "b4295bfde06222d42d52fbc4aca04e6af0b1f9a3ec4e9310d0e706aedf4967c3"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/menv/releases/download/0.5.0/menv-0.5.0-linux-amd64.tar.gz"
-      sha256 "a6d8ff35ad25d8b21d2bfb67b81e59ebfa83cf288e1e7931c735fc39ba9f3096"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.6.0/menv-0.6.0-linux-amd64.tar.gz"
+      sha256 "c3945bf41cd077454099d4aeeb81f4b29c0d82dd83b494f04cfda3bfebe40b40"
     end
   end
 
