dbc0d0031e3ca055cdc800ce5680ed61f9a3e4ca
thecheerfuldev <mark@markhendriks.nl>
2024-02-12 14:47:02 +0100

    Updated menv to 0.9.2

--- Formula/menv.rb
+++ Formula/menv.rb
@@ -9,22 +9,22 @@ class Menv < Formula
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.1/menv-0.9.1-darwin-arm64.tar.gz"
-      sha256 "2b339a8840c275aa502949bb0dce209c0340a0fd9c126f6bbb1d084e144739db"
+      sha256 "06eb564225a7e0e36ac6c804adc1057eefbd04e5f742c95352c501362ab443fc"
     end
     if Hardware::CPU.intel?
       url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.1/menv-0.9.1-darwin-amd64.tar.gz"
-      sha256 "a9f528243b585756371bec6f648ba62ff6309bec4ae45b33fed9d629cff3a9b3"
+      sha256 "0d6a30d78179d237f125b93cc6d8be6b47edfdf33c3240294e833ac2a62098b8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.1/menv-0.9.1-linux-arm64.tar.gz"
-      sha256 "9d2f1106f73fbc1790fd667856f1e2b8d0c527edffdd9307269c2ba1b0fe58a8"
+      sha256 "46d28a2aaabbb12028ac66b6cde51ca6f59d57eeb09d71b4379e4d9bb445f057"
     end
     if Hardware::CPU.intel?
       url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.1/menv-0.9.1-linux-amd64.tar.gz"
-      sha256 "16e8b9ee8b5cd6e2dd0c8d9973f60ca6bf82fe44322fe6a66a0146138d5f9cd9"
+      sha256 "666a3fce15775682d6e0068eede326c567dca82e3844c9caae9e29d0f96d8b68"
     end
   end
 
