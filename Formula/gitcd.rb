2aa39cf153ccd5484e9a1169b30574b8b934a9b8
thecheerfuldev <mark@markhendriks.nl>
2024-02-21 15:39:56 +0100

    Updated gitcd to 1.0.5

--- Formula/gitcd.rb
+++ Formula/gitcd.rb
@@ -2,28 +2,28 @@ class Gitcd < Formula
   desc "Index and navigate to your git projects"
   homepage "https://github.com/thecheerfuldev/gitcd-go"
   license "Apache-2.0"
-  version "1.0.4"
+  version "1.0.5"
 
   head "https://github.com/thecheerfuldev/workspace-manager.git"
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/1.0.4/gitcd-go-1.0.4-darwin-arm64.tar.gz"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/1.0.5/gitcd-go-1.0.5-darwin-arm64.tar.gz"
       sha256 "1cdc23981cb571857fbf6cb2d91f11199b28e27aae6fa3cb0a4cdb25a91339a4"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/1.0.4/gitcd-go-1.0.4-darwin-amd64.tar.gz"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/1.0.5/gitcd-go-1.0.5-darwin-amd64.tar.gz"
       sha256 "4f5245c7e9f875c48a6fc5b0a1fae66761e667d1f6ed6026e5cce939aee8a9e8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/1.0.4/gitcd-go-1.0.4-linux-arm64.tar.gz"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/1.0.5/gitcd-go-1.0.5-linux-arm64.tar.gz"
       sha256 "4faddf329313b4be54d88629a4650a22d78613d5eabb9f0253347b472315a91c"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/1.0.4/gitcd-go-1.0.4-linux-amd64.tar.gz"
+      url "https://github.com/TheCheerfulDev/gitcd-go/releases/download/1.0.5/gitcd-go-1.0.5-linux-amd64.tar.gz"
       sha256 "04bbec0e440a7625698aedb5339b2bd7c9ccdfe3d1eea1a5ec542c4e6eb6ce82"
     end
   end
