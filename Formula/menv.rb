ea1e481e2db9bcd75f77d048af9e509f63fd651b
thecheerfuldev <mark@markhendriks.nl>
2024-06-18 07:50:58 +0200

    Updated menv to 0.9.3

--- Formula/menv.rb
+++ Formula/menv.rb
@@ -2,29 +2,29 @@ class Menv < Formula
   desc "Manage your mvn configurations"
   homepage "https://github.com/thecheerfuldev/menv"
   license "Apache-2.0"
-  version "0.9.2"
+  version "0.9.3"
 
   head "https://github.com/thecheerfuldev/menv.git"
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.2/menv-0.9.2-darwin-arm64.tar.gz"
-      sha256 "06eb564225a7e0e36ac6c804adc1057eefbd04e5f742c95352c501362ab443fc"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.3/menv-0.9.3-darwin-arm64.tar.gz"
+      sha256 "98ae064df3deafc84f835a6c6c98bbbe9cccddb252348fbfb8f2dcf0aa799579"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.2/menv-0.9.2-darwin-amd64.tar.gz"
-      sha256 "0d6a30d78179d237f125b93cc6d8be6b47edfdf33c3240294e833ac2a62098b8"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.3/menv-0.9.3-darwin-amd64.tar.gz"
+      sha256 "b37a8bc619d8d6a085cdf4a9160de422169d0b4552a50cf8d6678c12753749cd"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.2/menv-0.9.2-linux-arm64.tar.gz"
-      sha256 "46d28a2aaabbb12028ac66b6cde51ca6f59d57eeb09d71b4379e4d9bb445f057"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.3/menv-0.9.3-linux-arm64.tar.gz"
+      sha256 "b8a93b96b8b7e8081620a37729e06fb413282a7e22c68ecbbbdb813ee993dd16"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.2/menv-0.9.2-linux-amd64.tar.gz"
-      sha256 "666a3fce15775682d6e0068eede326c567dca82e3844c9caae9e29d0f96d8b68"
+      url "https://github.com/TheCheerfulDev/menv/releases/download/0.9.3/menv-0.9.3-linux-amd64.tar.gz"
+      sha256 "f7b07c3485704bd17c23a8217e71d90cfa3abc069b21bda617fbdd35e595c974"
     end
   end
 
