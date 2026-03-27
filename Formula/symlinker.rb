f47b6e0616402b1af8690c5a3100aebd49cdb8ad
thecheerfuldev <mark@markhendriks.nl>
2026-03-27 07:37:50 +0100

    symlinker v1.0.1

--- Formula/symlinker.rb
+++ Formula/symlinker.rb
@@ -2,29 +2,29 @@ class Symlinker < Formula
   desc "Make symlinking your dotfiles a breeze."
   homepage "https://github.com/thecheerfuldev/symlinker"
   license "Apache-2.0"
-  version "1.0.0"
+  version "1.0.1"
 
   head "https://github.com/thecheerfuldev/symlinker.git"
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/symlinker/releases/download/1.0.0/symlinker-1.0.0-darwin-arm64.tar.gz"
-      sha256 "78f5362e3c8a92fea269075422716c581782f66b49d923656f60a42135cc6c0b"
+      url "https://github.com/TheCheerfulDev/symlinker/releases/download/1.0.1/symlinker-1.0.1-darwin-arm64.tar.gz"
+      sha256 "cb01c635eab6c514f7163f76c4db60056b42519877a7106fc40b9eafbe1465e9"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/symlinker/releases/download/1.0.0/symlinker-1.0.0-darwin-amd64.tar.gz"
-      sha256 "048ef2597e3661c0a40303b5af7b1872d0ff97ce22c2129fc2b412d2b5c00dfd"
+      url "https://github.com/TheCheerfulDev/symlinker/releases/download/1.0.1/symlinker-1.0.1-darwin-amd64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/TheCheerfulDev/symlinker/releases/download/1.0.0/symlinker-1.0.0-linux-arm64.tar.gz"
-      sha256 "6bc5fa05799b82cc0a7a60110293b2f7271806c48a62a3178a35ff33a2bf2d0f"
+      url "https://github.com/TheCheerfulDev/symlinker/releases/download/1.0.1/symlinker-1.0.1-linux-arm64.tar.gz"
+      sha256 "d11b575ecfb7f8de7485d7844d3057fb163913d8f8471bb27130a3462d3a7f8c"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/TheCheerfulDev/symlinker/releases/download/1.0.0/symlinker-1.0.0-linux-amd64.tar.gz"
-      sha256 "cec11d4bcc3c455ff43a4894efc64ca6939886aaa17088e92a4b757a3d92c07f"
+      url "https://github.com/TheCheerfulDev/symlinker/releases/download/1.0.1/symlinker-1.0.1-linux-amd64.tar.gz"
+      sha256 "8ae54525068d46a757a60acb848e76a71d807c8b9e65fc2cb7391e40052d2480"
     end
   end
 
